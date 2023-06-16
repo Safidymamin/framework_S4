443c39c6e34bc0e1d6defda20191daf98fc6293f
Safidymamin <118508017+Safidymamin@users.noreply.github.com>
2023-06-16 09:58:49 +0300

    Sprint1

---
+++ FrontServlet.java
@@ -0,0 +1,8 @@
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontServlet extends HttpServlet{
+    
+}
