5a94a49ae4c2ea6867627fa57219b0d4c5a640b1
Safidymamin <118508017+Safidymamin@users.noreply.github.com>
2023-07-03 09:11:00 +0300

    Sprint2

--- FrameWork/src/main/java/etu2003/framework/servlet/FrontServlet.java
+++ FrameWork/src/main/java/etu2003/framework/servlet/FrontServlet.java
@@ -1,13 +1,16 @@
-package etu2003framework.servlet;
+package etu2003.framework.servlet;
 
+import etu2003.framework.Mapping;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 //@WebServlet(name = "FrontServlet", value = "/*")
 public class FrontServlet extends HttpServlet {
+    HashMap<String, Mapping> MappingUrl;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
