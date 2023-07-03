36d6b51e5158c47677bb9ba5c0e806d99ffdede6
Safidymamin <118508017+Safidymamin@users.noreply.github.com>
2023-07-03 09:31:23 +0300

    Sprint3

---
+++ FrameWork/src/main/java/etu2003/annotation/Url.java
@@ -0,0 +1,12 @@
+package etu2003.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Url {
+    String value();
+}
--- FrameWork/src/main/java/etu2003/framework/servlet/FrontServlet.java
+++ FrameWork/src/main/java/etu2003/framework/servlet/FrontServlet.java
@@ -1,6 +1,7 @@
 package etu2003.framework.servlet;
 
 import etu2003.framework.Mapping;
+import etu2003.framework.Util;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -10,7 +11,16 @@ import java.util.HashMap;
 
 //@WebServlet(name = "FrontServlet", value = "/*")
 public class FrontServlet extends HttpServlet {
-    HashMap<String, Mapping> MappingUrl;
+    HashMap<String, Mapping> mappingUrls;
+
+    public void init() throws ServletException {
+        String packageName = getServletContext().getInitParameter("packageName");
+        try{
+            Util.traitementMappingUrls(this.mappingUrls, packageName);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
