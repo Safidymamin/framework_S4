1b37e6fd0e9c2df5c328a41f6154add5471c6aa6
Safidymamin <118508017+Safidymamin@users.noreply.github.com>
2023-07-04 11:30:02 +0300

    Sprint7

--- framework/FrontServlet.java
+++ framework/FrontServlet.java
@@ -4,6 +4,8 @@ import etu2003.framework.Mapping;
 import etu2003.framework.utils.Fonctions;
 import etu2003.annotation.url;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URL;
 
@@ -28,40 +30,87 @@ public class FrontServlet extends HttpServlet{
         }
     }
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
         response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
 // sprint5
         ModelView modelView = Fonctions.recup_ModelView(mappingUrls, request, response);
-// sprint6
+// sprint6: request.setAttribute(key, (Object)valeurObjet);
         Fonctions.recuperationData(modelView, request, response);
+        
+        
+        
+        try {
+            Object myObject = Fonctions.getMyObject(mappingUrls, request, response);
+            // sprint 7: maka données formulaire
+            myObject = Fonctions.recuperationInputData(myObject, request, response);
+            afficherDetailClass(myObject, out); 
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace(out);
+        }
+        
+        
+        String packageName = getServletContext().getInitParameter("packageName");
+        URL root = Thread.currentThread().getContextClassLoader().getResource(packageName.replace(".", "//")); 
+        out.println("Front Servlet");   
+        out.println("Root: " + root);
+        for(String key : mappingUrls.keySet()){
+            Mapping mapping = mappingUrls.get(key);
+            out.println("<br>"); 
+            out.println("Cle: " + key + ", ClassName: "+ mapping.getClassName() + ", Mapping: " + mapping.getMethod());
+        }
+
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("/"+modelView.getUrl());
         requestDispatcher.forward(request, response);
         
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
         PrintWriter out = response.getWriter();
-        String packageName = getServletContext().getInitParameter("packageName");
-        URL root = Thread.currentThread().getContextClassLoader().getResource(packageName.replace(".", "//")); 
-        out.println("Front Servlet");   
-        out.println("Root: " + root);
-         for(String key : mappingUrls.keySet()){
-            Mapping mapping = mappingUrls.get(key);
-            out.println("Cle: " + key + ", ClassName: "+ mapping.getClassName() + ", Mapping: " + mapping.getMethod());
-            out.println("<br>"); 
+        try {
+            processRequest(request, response);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace(out);
         }
     }
 
-    // protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    //     processRequest(request, response);
-
-    // }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        processRequest(request, response);
-        
-        
+        PrintWriter out = response.getWriter();
+        try {
+            processRequest(request, response);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace(out);
+        }
+    }
+
+    protected void afficherDetailClass(Object object, PrintWriter out)throws ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException,InvocationTargetException {
+        out.println("----Debut my object ------------------------------------------------------------------myObject------------------------------------------------------------------");
+            Field[] fields = object.getClass().getDeclaredFields();
+            Method[] methods = object.getClass().getDeclaredMethods();
+            // Method metho =object.getClass().getMethod("setNom",String.class);
+            // metho.invoke(object, "Anthony");
+            out.println("<br>");
+            out.println("Field.length: "+ fields.length);
+            for (Field field : fields) {
+                field.setAccessible(true);
+                out.println("<br>");
+                out.println(field.getName()+" = "+(String)field.get(object));
+            }
+            out.println("<br>");
+            out.println("Method.length: "+ methods.length);
+            for (Method method : methods) {
+                method.setAccessible(true);
+                out.println("<br>");
+                out.println(method.getName());
+            }
+            out.println("<br>");
+            out.println("----fin my object ------------------------------------------------------------------myObject------------------------------------------------------------------");
+            out.println("<br>");
     }
 
 
--- test-framework/WEB-INF/classes/Emp.java
+++ test-framework/WEB-INF/classes/Emp.java
@@ -1,5 +1,4 @@
 package models;
-
 import etu2003.annotation.url;
 import etu2003.framework.servlet.ModelView;
 
@@ -7,7 +6,30 @@ public class Emp{
     String nom;
     String prenom;
 
-// sprint5: maka modelView anaovana dispatcher
+    public Emp(){
+    }
+
+    // sprint7: maka donnée formulaire atao anaty Classe
+    @url(value="emp-recupInputDonnee")
+    public ModelView getInputDonnees(){
+        ModelView modelView = new ModelView();
+        modelView.setUrl("recupInputDonnee.jsp");
+        return modelView;
+    }
+
+    // sprint6: maka donnée anle modelView ho dispatchena
+    @url(value="emp-recupDonnee")
+    public ModelView getDonnees(){
+        ModelView modelView = new ModelView();
+        modelView.setUrl("recupDonnee.jsp");
+        modelView.addItem("nom","Randriarimalala");
+        modelView.addItem("prenom","Anthony");
+        String nomComplet = this.nom +" "+ this.prenom;
+        return modelView;
+    }
+
+
+    // sprint5: maka modelView anaovana dispatcher
     @url(value="emp-nomComplet")
     public ModelView getNomComplet(){
         ModelView modelView = new ModelView();
@@ -15,14 +37,17 @@ public class Emp{
         return modelView;
     }
 
-// sprint6: maka donnée anle modelView ho dispatchena
-    @url(value="emp-recupDonnee")
-    public ModelView getDonnees(){
-        ModelView modelView = new ModelView();
-        modelView.setUrl("recupDonnee.jsp");
-        modelView.addItem("nom","Rakotondrasoa");
-        modelView.addItem("prenom","Zety");
-        String nomComplet = this.nom +" "+ this.prenom;
-        return modelView;
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenom() {
+        return prenom;
     }
 }
